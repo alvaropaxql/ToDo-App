0c623d8200d64cf9493e12d2a58ddc7f3bc0ba13
alvaropaxql <alvaropasnu@gmail.com>
2021-09-08 12:43:10 +0200

    React app added

---
+++ to-do-react-app/README.md
@@ -0,0 +1,11 @@
+# 2Do List React App
+Todo list app made in React. New todos can be created or added to the old ones, checked as completed and removed from the list. Todos can also be filtered by 'completed' and 'uncompleted'.
+
+#### *Screenshot*
+
+<p align="center">
+    <img src="screenshots\2do-list-react.png" alt="Todo List app" width="500"/>
+</p>
+
+---
+The implementation is based on a tutorial made by [Dev ED](https://developedbyed.com) with a few slight changes. A few more features and corrections will be added in the future.
---
+++ to-do-react-app/src/App.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+// import componets
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  // states
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // effect
+  useEffect(() => {
+    getLocalTodos();
+  }, []); // used just once
+
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]); // used multiple times, listen to the second argument's changes
+
+  // functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>2Do List</h1>
+      </header>
+      <div className="wave">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            className="shape-fill"
+          ></path>
+        </svg>
+      </div>
+
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
